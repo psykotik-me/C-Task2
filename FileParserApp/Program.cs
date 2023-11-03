00ce86b35367b7965a2df35e60a2c79b78e7f2f5
Psykotik <cube.namaste@gmail.com>
2023-11-03 18:48:56 +0200

    версія 0.2 без логування та тесування

--- FileParserApp/Program.cs
+++ FileParserApp/Program.cs
@@ -6,20 +6,21 @@ class Program
 {
     static void Main()
     {
-        // Set the desired culture (e.g., en-US for US culture with "." as the decimal separator)
-        CultureInfo customCulture = new CultureInfo("en-US");
-        // Set the culture for the current thread
-        CultureInfo.DefaultThreadCurrentCulture = customCulture;
-        CultureInfo.DefaultThreadCurrentUICulture = customCulture;
-        Console.WriteLine("Current Decimal Separator: " + customCulture.NumberFormat.CurrencyDecimalSeparator);
 
         Console.WriteLine("Введіть ім'я файлу з даними:");
         string input = Console.ReadLine();
+        while (input == "") input = Console.ReadLine();
+
         FileParser fp = new FileParser();
-        (int row, float sum) res;
-        res =  fp.GetBiggestAmountOfRow(input);
-        if (res.sum == 0 && res.row == 0) Console.WriteLine($"У вказаному файлі {input} немає жодного рядка, де є тільки числа(");
-        else Console.WriteLine("Максимальне значення: " + res.sum + " у рядку номер " + res.row);
+
+        ResOut res =  fp.GetBiggestAmountOfRow(input);
+        if (res.result.sum == 0 && res.result.row == 0) Console.WriteLine($"У вказаному файлі {input} немає жодного рядка, де є тільки числа(");
+        else //Console.WriteLine("Максимальне значення: " + res.result.sum + " у рядку номер " + res.result.row);
+        {
+            res.printResult();
+            Console.WriteLine("Введіть '1' щоб побачити неправильні рядки:");
+            if (Console.ReadLine().Equals("1"))   res.printBadRows();
+        }
 
     }
 
